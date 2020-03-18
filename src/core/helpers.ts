36235bc4c20b72b74a4d65e1da649b97b7494e9a
Jefferson Rylee <rylee.jeff385@gmail.com>
2020-03-18 21:55:18 +0800

    Add calculateIp helper

--- src/core/helpers.ts
+++ src/core/helpers.ts
@@ -23,6 +23,42 @@ export function calculateCapacity(subnetMask: number) {
   return Math.pow(256, 4 - subnetMask / 8) - 2;
 }
 
+export function isIpNumInvalid(num: number) {
+  return num >= 256 || num < 0;
+}
+
+export function calculateIp(address: IPv4, n: number): Address4 {
+  const ip: Address4 = createIPv4(address);
+  const newIp: number[] = ip.parsedAddress.map(n => +n);
+
+  // add to host
+  newIp[3] += Math.round(n);
+
+  do {
+    // add/subtract 256 to current, add/subtract 1 to previous
+    // until everything is valid and within 0 <= n < 256
+    for (let i = 3; i > 0; i--) {
+      const mult = newIp[i] >= 256 ? -1 : newIp[i] < 0 ? 1 : 0;
+      newIp[i] += mult * 256;
+      newIp[i - 1] -= mult;
+    }
+  } while (
+    // if a number is invalid, continue do while loop
+    newIp.some(isIpNumInvalid) &&
+    !isIpNumInvalid(newIp[0])
+  );
+
+  // try to create ip
+  const newIpValue: string = newIp.join('.');
+  try {
+    return createIPv4(newIpValue);
+  } catch (_) {
+    throw new Error(
+      `Resulting IP address is an invalid IP address: ${newIpValue}`
+    );
+  }
+}
+
 export function verifyHosts(hosts: number[], capacity: number): boolean {
   const total: number = hosts.reduce(
     (total, host) => total + (host > 0 ? host + 2 : 0),
