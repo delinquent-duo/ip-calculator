9a2e086de7a2d005d1a46172fa777e3c8da9c28c
Jefferson Rylee <rylee.jeff385@gmail.com>
2020-03-28 19:40:12 +0800

    temp: expose Network and core helpers

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import { Network } from './core/Network';
+import * as H from './core/helpers';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+(window as any).Network = Network;
+(window as any).H = H;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
